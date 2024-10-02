a73d81ac1ab2ef63a77e574e1967f7b261cc9121
Sanjana-Desh <aleena.selan@gmail.com>
2024-10-02 14:34:33 +0530

    chnaged

--- Learn_the_basics/Things_to_know_in_cpp/practice.cpp
+++ Learn_the_basics/Things_to_know_in_cpp/practice.cpp
@@ -1,19 +1,25 @@
-#include<iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int main()
+{
+    
+    vector<vector<vector<int>>> v1(4, vector<vector<int>> (3, vector<int> (2,0)));
 
-int main() {
-
-    int n;
-    cin >> n;
-    int i;
-    i = 10;
-    while (i>0)
+    for ( auto i : v1)
     {
-        cout << n*i << endl;
-        i = i-1;
-    }
-    return 0;
+        for ( auto j: i)
+        {
+            for ( auto k: j)
+            {
+                cout << k;
+            }
 
+            cout << endl;
+        }
+
+        cout << endl;
+    }
+    
 }
